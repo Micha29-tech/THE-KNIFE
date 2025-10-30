69df6b7a7a43954aca8e3b93d0d8628037010dd0
Micha29-tech <michaelascafdi@gmail.com>
2025-10-30 23:47:08 +0100

    Sistemata struttura progetto:aggiornati package spostata cartella theknife

--- src/theknife/TheKnife.java
+++ src/theknife/TheKnife.java
@@ -1,6 +1,5 @@
 package theknife;
 import java.util.Scanner;
-import theknife.model.Ristorante;
 
 public class TheKnife {
     public static void main(String[] args) {
@@ -8,34 +7,40 @@ public class TheKnife {
         
         // -----TEST : Creo un ristorante-----
         Ristorante r1 = new Ristorante("La Ruota", "Italia", "Gallarate", "Via Giacomo Leopardi 4", "Ristorante italiano/pizzeria", 20, true, true);
-        System.out.println(r1);
+        System.out.println(r1.toString());
         System.out.println();
+        while(true){
+            // -----------------------------------------------------
+            System.out.println("======= THE KNIFE =======");
+            System.out.println("1) Registrati");
+            System.out.println("2) Accedi");            
+            System.out.println("3) Continua come ospite");
+            System.out.println("0) Esci");
+            
+            System.out.println("Scelta: ");
+            int scelta = in.nextInt();
+            in.nextLine();
 
-        // -----------------------------------------------------
-        System.out.println("======= THE KNIFE =======");
-        System.out.println("1) Accedi");
-        System.out.println("2) Registrati");
-        System.out.println("3) Continua come ospite");
-        System.out.println("0) Esci");
-        
-        System.out.println("Scelta: ");
-        int scelta = in.nextInt();
-        in.nextLine();
-
-        switch (scelta) {
-            case 1:
-                System.out.println("Funzione login...");
-                break;
-            case 2:
-                System.out.println("Funzione registrazione...");
-                break;
-            case 3:
-                System.out.println("Accesso come ospite...");
-                break;
-            default:
-                System.out.println("Ciao!");
-                break;
+            switch (scelta) {
+                case 1:
+                    System.out.println("Funzione login...");
+                    break;
+                case 2:
+                    System.out.println("Funzione registrazione...");
+                    break;
+                case 3:
+                    System.out.println("Accesso come ospite...");
+                    break;
+                case 0:
+                    System.out.println("Uscita in corso...");
+                    in.close();
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Ciao!");
+                    break;
+            }
+            //in.close();
         }
-        in.close();
     }
 }
--- src/theknife/model/Recenzione.java
+++ src/theknife/model/Recenzione.java
@@ -1,4 +1,4 @@
-package model;
+package theknife.model;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
